4d9b6b5f63cc2fbed108036ed3fa247e562efb5c
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-04-28 19:58:25 +0800

    Building the Overall Layout

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <div className="container">
+      <div className="sidebar">Sidebar</div>
+
+      <header className="header">Header</header>
+
+      <div className="realtors">Top 3 realtors</div>
+
+      <section className="features">Features</section>
+
+      <div className="story__pictures">Story pictures</div>
+
+      <div className="story__content">Story content </div>
+
+      <section className="homes">Homes</section>
+
+      <section className="gallery">Gallery</section>
+
+      <footer className="footer">Footer</footer>
+    </div>
+  );
+};
+
+export default Layout;
