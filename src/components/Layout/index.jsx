e6d5059c84c9d08a0e823d6f47ff1fdc1bf2cb24
Chelzea Salvador <chelzeasalvador@gmail.com>
2022-02-23 09:43:55 +0800

    fix build issue

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -148,32 +148,32 @@ const Layout = () => {
       <footer className="footer">
         <ul className="nav">
           <li className="nav-item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Find your dream home
             </a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Request proposal
             </a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Download home planner
             </a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Contact us
             </a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Submit your property
             </a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav__link">
+            <a href="/" className="nav__link">
               Come work with us!
             </a>
           </li>
